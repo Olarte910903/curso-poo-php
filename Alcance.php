a4ac92e5d45da1a55eb106b74fee36ad2ffb1ef5
Miguel Olarte <lmolarteb@unadvirtual.edu.co>
2023-12-26 20:00:03 -0500

    alcance o encapsulamiento

---
+++ Alcance.php
@@ -0,0 +1,15 @@
+<?php
+
+class User
+{
+    //public
+    //protected
+    //private
+
+    public const PAGINATE = 25;
+
+    protected const NUMERO_PI = 3.14;
+
+    private const NOMBRE = "Miguel";
+    
+}
--- interface/StoreInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-interface StoreInterface
-{
-    public function get(); //declaramos lo que queremos
-}
